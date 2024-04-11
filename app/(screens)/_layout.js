2cfb408ca7c6761513a7ee72192d93dc53fdcb01
pratham-jaiswal <prathamj0502@gmail.com>
2024-04-12 01:52:22 +0530

    Setup: Settings screen

---
+++ app/(screens)/_layout.js
@@ -0,0 +1,29 @@
+import { Stack, router } from "expo-router";
+import { Pressable } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const RootLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#7469B6",
+        },
+        headerTintColor: "#FFE6E6",
+        headerTitleStyle: {
+          fontSize: 20,
+        },
+      }}
+    >
+      <Stack.Screen
+        name="settings"
+        options={{
+          headerShown: true,
+          headerTitle: "Settings",
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default RootLayout;
