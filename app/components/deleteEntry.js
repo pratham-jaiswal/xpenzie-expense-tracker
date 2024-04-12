437a41c09c9082beddd0859ed00b3eacc1066fc1
pratham-jaiswal <prathamj0502@gmail.com>
2024-04-12 22:27:52 +0530

    Add: Delete functionality

---
+++ app/components/deleteEntry.js
@@ -0,0 +1,157 @@
+import { useState } from "react";
+import { StyleSheet, Pressable, Text, View, Modal } from "react-native";
+
+const DeleteEntry = ({
+  db,
+  entries,
+  setEntries,
+  totalIncome,
+  totalExpenditure,
+  setTotalIncome,
+  setTotalExpenditure,
+  showDeletePrompt,
+  setShowDeletePrompt,
+  entryIdToDelete
+}) => {
+
+  const deleteEntry = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "DELETE FROM transaction_entries WHERE id = ?",
+        [entryIdToDelete],
+        (txObj, resultSet) => {
+          if(resultSet.rowsAffected > 0) {
+            let entry = entries.find((entry) => entry.id === entryIdToDelete);
+            let entryAmount = entry.amount;
+            console.log(entry)
+            let entryType = entry.type;
+            let existingEntries = [...entries].filter((entry) => entry.id !== entryIdToDelete);
+            if(entryType === "Expenditure") {
+              setTotalExpenditure(totalExpenditure - entryAmount);
+            }
+            else{
+              setTotalIncome(totalIncome - entryAmount);
+            }
+            setEntries(existingEntries);
+          }
+          handleCancelClick();
+        },
+        (txObj, error) => console.log(error)
+      );
+    });
+
+    // let entry = entries.find((entry) => entry.id === entryIdToDelete);
+    // console.log(entry)
+    // setShowDeletePrompt(false);
+    // setEntryIdToDelete(null);
+  };
+
+  const handleCancelClick = () => {
+    setShowDeletePrompt(false);
+  };
+
+  return (
+    <View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showDeletePrompt}
+        onRequestClose={() => {
+          setShowDeletePrompt(!showDeletePrompt);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.line1Container}>
+            <Text style={styles.line1text}>
+              Are you sure you want to delete the entry?
+            </Text>
+          </View>
+          <View style={styles.formButtonsContainer}>
+            <Pressable
+              style={({ pressed }) => [
+                {
+                  backgroundColor: pressed ? "#f5d7d7" : "#FFE6E6",
+                },
+                styles.deleteButton,
+              ]}
+              onPress={deleteEntry}
+            >
+              <Text style={styles.deleteButtonText}>Delete</Text>
+            </Pressable>
+            <Pressable style={styles.cancelButton} onPress={handleCancelClick}>
+              {({ pressed }) => (
+                <Text
+                  style={[
+                    { color: pressed ? "#f5d7d7" : "#FFE6E6" },
+                    styles.cancelButtonText,
+                  ]}
+                >
+                  Cancel
+                </Text>
+              )}
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    width: "70%",
+    marginHorizontal: "15%",
+    marginTop: "80%",
+    height: "20%",
+    justifyContent: "space-around",
+    backgroundColor: "rgba(116, 105, 182, 0.8)",
+    opacity: 0.99,
+    paddingVertical: "5%",
+    paddingHorizontal: "5%",
+    elevation: 5,
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    shadowColor: "black",
+    borderRadius: 10,
+  },
+  line1Container: {
+    flex: 1,
+    marginBottom: "3%",
+  },
+  line1text: {
+    fontSize: 18,
+    color: "#FFE6E6",
+    width: "100%",
+    textAlign: "center",
+  },
+  formButtonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginVertical: "3%",
+  },
+  cancelButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: "3%",
+    paddingRight: "7%",
+  },
+  cancelButtonText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  deleteButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: "7%",
+    borderRadius: 4,
+    elevation: 3,
+  },
+  deleteButtonText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#7469B6",
+  },
+});
+
+export default DeleteEntry;
