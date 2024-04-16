e082d1bc6c46246a15537b5735c4bef87f58070d
pratham-jaiswal <prathamj0502@gmail.com>
2024-04-17 01:42:45 +0530

    Add: Function to set currency; more settings options with no functions yet

--- app/(screens)/settings.js
+++ app/(screens)/settings.js
@@ -1,19 +1,201 @@
-import { StyleSheet, Pressable, Text, View } from "react-native";
+import { useCallback, useState } from "react";
+import {
+  StyleSheet,
+  Pressable,
+  Text,
+  View,
+  TouchableHighlight,
+} from "react-native";
+import Currency from "../components/currency";
+import Language from "../components/language";
+import Account from "../components/account";
+import { useFocusEffect } from "expo-router";
+import * as SecureStore from "expo-secure-store";
+
+async function save(key, value, reqAuth) {
+  await SecureStore.setItemAsync(key, value, {
+    requireAuthentication: reqAuth,
+  });
+}
+
+async function getValueFor(key, reqAuth) {
+  let result = await SecureStore.getItemAsync(key, {
+    requireAuthentication: reqAuth,
+  });
+  return result;
+}
 
 const Settings = () => {
+  const [showCurrencyForm, setShowCurrencyForm] = useState(false);
+  const [showAccountForm, setShowAccountForm] = useState(false);
+  const [showLanguageForm, setShowLanguageForm] = useState(false);
+
+  const [currencyValue, setCurrencyValue] = useState(null);
+  const [currencySymbol, setCurrencySymbol] = useState(null);
+  const [languageValue, setLanguageValue] = useState(null);
+  const [languageCode, setLanguageCode] = useState(null);
+
+  useFocusEffect(
+    useCallback(() => {
+      const fetchData = async () => {
+        let currVal = await getValueFor("currencyValue", false);
+        if (!currVal) {
+          await save("currencyValue", "65", false);
+          currVal = "65";
+          setCurrencyValue(currVal);
+        } else if (currVal !== currencyValue) {
+          setCurrencyValue(currVal);
+        }
+
+        let currSymbol = await getValueFor("currencySymbol", false);
+        if (!currSymbol) {
+          await save("currencySymbol", "₹", false);
+          currSymbol = "₹";
+          setCurrencySymbol(currSymbol);
+        } else if (currSymbol !== currencySymbol) {
+          setCurrencySymbol(currSymbol);
+        }
+
+        let langVal = await getValueFor("languageValue", false);
+        if (!langVal) {
+          await save("languageValue", "1", false);
+          langVal = "1";
+          setLanguageValue(langVal);
+        } else if (langVal !== languageValue) {
+          setLanguageValue(langVal);
+        }
+
+        let langCode = await getValueFor("languageCode", false);
+        if (!langCode) {
+          await save("languageCode", "en", false);
+          langCode = "en";
+          setLanguageCode(langCode);
+        } else if (langCode !== languageCode) {
+          setLanguageCode(langCode);
+        }
+      };
+      fetchData();
+    }, [])
+  );
+
+  const handlePress = (option) => {
+    console.log(option);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Settings</Text>
+      <TouchableHighlight
+        underlayColor="#0000000d"
+        style={styles.option}
+        onPress={() => setShowAccountForm(true)}
+      >
+        <Text style={styles.optionText}>Account</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        underlayColor="#0000000d"
+        style={styles.option}
+        onPress={() => setShowCurrencyForm(true)}
+      >
+        <Text style={styles.optionText}>Currency</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        underlayColor="#0000000d"
+        style={styles.option}
+        onPress={() => handlePress("D")}
+      >
+        <Text style={styles.optionText}>Backup & Restore</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        underlayColor="#0000000d"
+        style={styles.option}
+        onPress={() => handlePress("C")}
+      >
+        <Text style={styles.optionText}>Notification</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        underlayColor="#0000000d"
+        style={styles.option}
+        onPress={() => setShowLanguageForm(true)}
+      >
+        <Text style={styles.optionText}>Language</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        underlayColor="#0000000d"
+        style={styles.option}
+        onPress={() => handlePress("D")}
+      >
+        <Text style={styles.optionText}>Security</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        underlayColor="#0000000d"
+        style={styles.option}
+        onPress={() => handlePress("D")}
+      >
+        <Text style={styles.optionText}>Feedback</Text>
+      </TouchableHighlight>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Expense Tracker App v0.7.0</Text>
+      </View>
+
+      <Account showForm={showAccountForm} setShowForm={setShowAccountForm} />
+      <Currency
+        showForm={showCurrencyForm}
+        setShowForm={setShowCurrencyForm}
+        save={save}
+        getValueFor={getValueFor}
+        currencySymbol={currencySymbol}
+        currencyValue={currencyValue}
+        setCurrencySymbol={setCurrencySymbol}
+        setCurrencyValue={setCurrencyValue}
+      />
+      <Language
+        showForm={showLanguageForm}
+        setShowForm={setShowLanguageForm}
+        save={save}
+        getValueFor={getValueFor}
+        languageCode={languageCode}
+        languageValue={languageValue}
+        setLanguageCode={setLanguageCode}
+        setLanguageValue={setLanguageValue}
+      />
     </View>
+
+    // TO-DO
+    // Priority 1 - Currency Symbols
+    // Priority 4 - Language
+    // Priority 2 - Customed Categories
+    // Priority 3 - Notification settings - Daily reminder to log transactions, daily recap, weekly recap, monthly recap, yearly recap, financial insights
+    // Priority 2 - Backup to gdrive
+    // Priority 1 - Restore from gdrive
+    // Priority 4 - Passcode or biometric authentication to access the app
+    // Priority 5 - Feedback and support
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    width: "90%",
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "#AD88C6"
+    alignSelf: "center",
+  },
+  option: {
+    width: "100%",
+    borderBottomWidth: 1,
+    borderBottomColor: "#f2cece",
+    paddingVertical: 20,
+  },
+  optionText: {
+    fontSize: 18,
+    color: "#FFE6E6",
+    fontWeight: "bold",
+  },
+  footer: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+  footerText: {
+    fontSize: 12,
+    color: "#f2cece",
   },
 });
 
